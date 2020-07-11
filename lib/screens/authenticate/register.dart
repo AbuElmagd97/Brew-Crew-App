4df5970560a7e17feb17192da2100090581329f5
Mohamed.AbuElmagd <mohamed.aboelmagd2012@gmail.com>
2020-07-11 02:11:15 +0200

    Register and signIn with email and password

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -3,15 +3,16 @@ import 'package:brewcrew/shared/constants.dart';
 import 'package:brewcrew/shared/loading.dart';
 import 'package:flutter/material.dart';
 
-class SignIn extends StatefulWidget {
+class Register extends StatefulWidget {
   final Function toggleView;
-  SignIn({this.toggleView});
+
+  Register({this.toggleView});
 
   @override
-  _SignInState createState() => _SignInState();
+  _RegisterState createState() => _RegisterState();
 }
 
-class _SignInState extends State<SignIn> {
+class _RegisterState extends State<Register> {
   final AuthService _auth = AuthService();
   final _formKey = GlobalKey<FormState>();
   bool loading = false;
@@ -21,7 +22,6 @@ class _SignInState extends State<SignIn> {
   String password = '';
   String error = '';
 
-
   @override
   Widget build(BuildContext context) {
     return loading ? Loading() : Scaffold(
@@ -29,11 +29,11 @@ class _SignInState extends State<SignIn> {
       appBar: AppBar(
         backgroundColor: Colors.brown[400],
         elevation: 0.0,
-        title: Text('Sign in to Brew Crew'),
+        title: Text('Sign up to Brew Crew'),
         actions: <Widget>[
           FlatButton.icon(
             icon: Icon(Icons.person),
-            label: Text('Register'),
+            label: Text('Sign In'),
             onPressed: () {
               widget.toggleView();
             },
@@ -58,7 +58,7 @@ class _SignInState extends State<SignIn> {
               TextFormField(
                 decoration: textInputDecoration.copyWith(hintText: 'Password'),
                 validator: (val) =>
-                val.length < 6 ? 'Enter a password 6+ chars long ' : null,
+                    val.length < 6 ? 'Enter a password 6+ chars long ' : null,
                 obscureText: true,
                 onChanged: (val) {
                   setState(() => password = val);
@@ -71,10 +71,11 @@ class _SignInState extends State<SignIn> {
                     setState(() {
                       loading = true;
                     });
-                   dynamic result = await _auth.signInWithEmailAndPassword(email, password);
+                    dynamic result = await _auth.registerWithEmailAndPassword(
+                        email, password);
                     if (result == null) {
-                      setState(() {
-                        error = 'Invalid credentials';
+                      setState((){
+                        error = 'please enter a valid email';
                         loading = false;
                       });
                     }
@@ -82,7 +83,7 @@ class _SignInState extends State<SignIn> {
                 },
                 color: Colors.pink[400],
                 child: Text(
-                  'Sign in',
+                  'Register',
                   style: TextStyle(
                     color: Colors.white,
                   ),
