6ff325aa22581d12aa46f39257060cd9d2e4ad38
Mohamed.AbuElmagd <mohamed.aboelmagd2012@gmail.com>
2020-07-14 03:30:45 +0200

    create brew model create BrewList & BrewTile & SettingsForm Widgets test app & Wrap up

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,3 +1,5 @@
+import 'package:brewcrew/models/brew.dart';
+import 'package:brewcrew/models/user.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class DatabaseService {
@@ -7,7 +9,7 @@ class DatabaseService {
 
   // collection reference
   final CollectionReference brewCollection =
-      Firestore.instance.collection('brews');
+  Firestore.instance.collection('brews');
 
   Future updateUserData(String sugars, String name, int strength) async {
     return await brewCollection.document(uid).setData({
@@ -16,4 +18,38 @@ class DatabaseService {
       'strength': strength,
     });
   }
+
+  // brew list from snapshot
+  List<Brew> _brewListFromSnapshot(QuerySnapshot snapshot) {
+    return snapshot.documents.map((doc) {
+      return Brew(
+          name: doc.data['name'] ?? '',
+          strength: doc.data['strength'] ?? 0,
+          sugars: doc.data['sugars'] ?? '0'
+      );
+    }).toList();
+  }
+
+  // userData from snapshot
+  UserData _userDataFromSnapshot(DocumentSnapshot snapshot) {
+    return UserData(
+      uid: uid,
+      name: snapshot.data['name'],
+      sugars: snapshot.data['sugars'],
+      strength: snapshot.data['strength'],
+    );
+  }
+
+  //get brews stream
+  Stream<List<Brew>> get brews {
+    return brewCollection.snapshots()
+        .map(_brewListFromSnapshot);
+  }
+
+  // get user doc stream
+  Stream<UserData> get userData {
+    return brewCollection.document(uid).snapshots()
+        .map(_userDataFromSnapshot);
+  }
+
 }
